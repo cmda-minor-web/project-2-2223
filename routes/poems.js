52e34ad66746e01a35f94a0cc72e2b7282a46eeb
K3A101 <keisha.alexander@hva.nl>
2023-04-12 10:50:44 +0200

    API setup

--- routes/poems.js
+++ routes/poems.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const request = require('request');
 const router = express.Router();
+const API_KEY = 'dichter-a6a8ae46-4c90'
+const options = {
+    url: 'https://dichter.responsible-it.nl/api/poetry?type=5&theme=mooi bos warme zomerdag',
+    headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json'
+    }
+}
 
 router.get('/', function (req, res) {
     res.render('index', {
@@ -9,11 +17,20 @@ router.get('/', function (req, res) {
 });
 
 router.get('/poems', (req, res)=>{
-    res.render('poems',{
-         title: 'Make your poems'
-    }
-  
-    )
+    request.get(options, (error, response, body)=>{
+        if(error){
+            res.send(error)
+            console.log(error)
+        }else {
+            console.log(body)
+            res.render('poems', {
+               title: 'Maak je gedichten',
+               poemsData: body 
+            });
+        }
+    })
+
 })
+
 
 module.exports = router;
